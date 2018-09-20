d08cd5594e09b6ae7dad0804cd9fbf631dc7c16a
Mohsen Esmaeili <m-esm@hotmail.com>
2018-09-20 13:40:16 +0430

    npm version

---
+++ dist/debug.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Start_1 = require("./Start");
+const _1 = require(".");
+const path_1 = require("path");
+_1.AuthService.configure({
+    tokenExpireIn: 1000 * 60 * 60,
+    smsProvider: 'SmsIrService'
+});
+_1.DbService.configure({
+    mongoDb: 'serendipTests',
+    mongoUrl: 'mongodb://localhost:27017'
+});
+Start_1.start({
+    cert: path_1.join(__dirname, '..', 'localhost_cert.pem'),
+    key: path_1.join(__dirname, '..', 'localhost_key.pem'),
+    staticPath: __dirname,
+    cpuCores: 1,
+    httpPort: 2020,
+    httpsPort: 2022,
+    httpsOnly: false,
+    controllers: [_1.AuthController, _1.ServerController],
+    services: [_1.SmsIrService, _1.EmailService, _1.DbService, _1.AuthService, _1.ViewEngineService]
+}).then(() => {
+    console.log('Server workers started successfully!');
+}).catch((err) => {
+    console.error(err);
+    process.exit();
+});
---
+++ dist/start.js
@@ -0,0 +1,50 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("./core");
+const cluster = require("cluster");
+const os_1 = require("os");
+const events_1 = require("events");
+function start(opts) {
+    if (!opts.controllers)
+        opts.controllers = [];
+    if (!opts.services)
+        opts.services = [];
+    var workerEmitter = new events_1.EventEmitter();
+    return new Promise((resolve, reject) => {
+        var cpuCount = 1;
+        //opts.cpuCores || cpus().length
+        if (opts.cpuCores)
+            if (opts.cpuCores == 'max')
+                cpuCount = os_1.cpus().length;
+            else
+                cpuCount = parseInt(opts.cpuCores.toString());
+        var stopForking = false;
+        // if this is process
+        if (cluster.isMaster && cpuCount > 1) {
+            var onClusterMsg = (msg) => {
+                if (msg == "fork")
+                    cluster.fork().on('message', onClusterMsg);
+            };
+            onClusterMsg('fork');
+            cluster.on('exit', function (worker) {
+                console.error('Worker %s has died! Creating a new one.', worker.id);
+                if (!stopForking)
+                    cluster.fork();
+            });
+        }
+        else {
+            core_1.Server.bootstrap(opts, cluster.worker || { id: 0 }, (err) => {
+                if (err)
+                    return reject(err);
+                if (cpuCount == 1)
+                    resolve();
+                else if (cluster.worker.id == cpuCount) {
+                    resolve(cluster.workers);
+                }
+                else if (cluster.worker.id < cpuCount)
+                    process.send('fork');
+            });
+        }
+    });
+}
+exports.start = start;
